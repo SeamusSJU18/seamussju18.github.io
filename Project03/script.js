ee4b0d141c4336d03729b077b80daf7c991b8b79
SeamusSJU18 <146009842+SeamusSJU18@users.noreply.github.com>
2024-03-30 10:28:45 -0500

    Update script.js

--- Project03/script.js
+++ Project03/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', async function() {
+document.addEventListener('DOMContentLoaded',() {
     let quizData = [];
     let playerName = '';
     let selectedQuiz = null;
