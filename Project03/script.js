83d8039c7f4bf439348a1e91ef1bbabcb2162849
SeamusSJU18 <146009842+SeamusSJU18@users.noreply.github.com>
2024-03-30 17:20:13 -0500

    Update script.js

--- Project03/script.js
+++ Project03/script.js
@@ -38,6 +38,7 @@ async function showEnterNameView() {
             startQuiz(currentQuizIndex);
         });
     });
+    reutrn false;
 }
 async function startQuiz(selectedQuizIndex) {
     const selectedQuiz = quizData.Quiz[selectedQuizIndex];
